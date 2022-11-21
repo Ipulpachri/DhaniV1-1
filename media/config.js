2c014bda0487a7eeb4689128e4d52f2c8e85b979
Xiaomi Team <112295130+XiaomiBOT@users.noreply.github.com>
2022-11-21 20:14:40 +0700

    Update config.js

--- media/config.js
+++ media/config.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['62895634865955'] //Masukin nomor mu
-global.ownernomer = "62895634865955" //Masukin nomor mu
-global.premium = ['62895634865955'] //Masukin nomor mu
-global.namanya = 'Langz' //Masukin nama mu
-global.fakename = '© LangBotz' //Masukin nama mu
-global.botname = 'LangBotz' //Masukin nama bot kamu
+global.owner = ['6285890316393'] //Masukin nomor mu
+global.ownernomer = "6285890316393" //Masukin nomor mu
+global.premium = ['6285890316393'] //Masukin nomor mu
+global.namanya = 'HanzOfc' //Masukin nama mu
+global.fakename = '© XiaomiBOT' //Masukin nama mu
+global.botname = 'XiaomiBOT' //Masukin nama bot kamu
 global.packname = 'Sticker By' //Gakusah di ubah kalau mau di ubah Monggo 
-global.author = 'LangBotz' //Monggo kalau mau ubah
+global.author = 'XiaomiBOT' //Monggo kalau mau ubah
 global.sessionName = 'KamuNanya' //Gak usah di ubah
 global.jumlha = '999' //Gak usah di ubah
 global.jumhal = '100000000000000' //Gak usah di ubah
@@ -25,9 +25,9 @@ global.gaya03 =  '⚔️' //Monggo kalau mau ubah
 global.gayar = '❏' //Monggo kalau mau ubah
 global.gayas = '❏' //Monggo kalau mau ubah
 global.scmu = 'Kamu Nanya??\nKamu Bertanya2?' //Monggo kalau mau ubah
-global.githubmu = 'https://github.com/GilangSan' //Isi nama github mu
-global.ytmu = 'https://youtube.com/channel/@WoolCream' //Gak usah di ubah
-global.ytvmu = 'https://youtube.com/' //Gak usah di ubah
+global.githubmu = 'https://github.com/XiaomiBOT' //Isi nama github mu
+global.ytmu = 'https://youtube.com/channel/@ranggabotzofc' //Gak usah di ubah
+global.ytvmu = 'https://youtube.com/@ranggabotzofc' //Gak usah di ubah
 global.mess = {
     success: '✅Done',
     admin: '_Fitur Hanya Dapat Di Gunakan Oleh Admin Group!_',
